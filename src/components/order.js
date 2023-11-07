6f3d01ca569f0be8d88b9e7f3d87298c3c3aaf82
Tomasz Rosko <tomasz.rosko@gmail.com>
2023-11-07 21:06:03 +0100

    deleting an order

--- src/components/order.js
+++ src/components/order.js
@@ -3,33 +3,40 @@ import React, {useEffect, useState} from 'react';
 function Order(props) {
 
 
-    const [cost, setCost] = useState(0)
-    const [order, setOrder] = useState([])
-    useEffect(() => {
-        if (props.newSandwich) {
-            setOrder(renderOrder => [...renderOrder, props.newSandwich])
-        }
-    }, [props.newSandwich])
-    useEffect(() => {
-        let costAll = order.reduce((sum, sandwich) => sum + sandwich.cost, 0)
-        setCost(costAll)
-    }, [order]);
-    return (
-        <div>
-            <h2>Złóż zamówienie</h2>{order.map((sandwich, index) => {
-            return (
-                <div key={index}>
-                    <h3>{index + 1}: {sandwich.size} kanapka
-                        (ilość dodatków: {sandwich.accessories.length})
-                        &nbsp; | &nbsp;
-                        {(sandwich.cost / 100).toFixed(2)}zł
-                    </h3>
-                </div>
-            )
-        })}<p>----------</p>
-            <p>Do zapłaty: {(cost / 100).toFixed(2)}zł</p>
+  const [cost, setCost] = useState(0)
+  const [order, setOrder] = useState([])
+  useEffect(() => {
+    if (props.newSandwich) {
+      setOrder(renderOrder => [...renderOrder, props.newSandwich])
+    }
+  }, [props.newSandwich])
+  useEffect(() => {
+    let costAll = order.reduce((sum, sandwich) => sum + sandwich.cost, 0)
+    setCost(costAll)
+  }, [order]);
+
+  const removeSandwich = (indx) => {
+      let myOrder = order.filter((sandwich, index) => index !== indx);
+      setOrder(myOrder);
+  }
+
+  return (
+    <div>
+      <h2>Złóż zamówienie</h2>{order.map((sandwich, index) => {
+      return (
+        <div key={index} className={"row-order"}>
+          <h3>{index + 1}: {sandwich.size} kanapka
+            (ilość dodatków: {sandwich.accessories.length})
+            &nbsp; | &nbsp;
+            {(sandwich.cost / 100).toFixed(2)}zł
+          </h3>
+          <div className="btn-order" onClick={() => removeSandwich(index)}>x</div>
         </div>
-    )
+      )
+    })}<p>----------</p>
+      <p>Do zapłaty: {(cost / 100).toFixed(2)}zł</p>
+    </div>
+  )
 }
 
 export default Order;
